d9547ae716dc35da7c15b0641d088e43a65ee5b4
ajdjyt <ajdjking@gmail.com>
2024-12-17 01:02:15 +0530

    fix text on about us

--- src/components/AboutUs.svelte
+++ src/components/AboutUs.svelte
@@ -7,10 +7,10 @@
         <h3 class="text-acc1 text-4xl sm:text-5xl md:text-6xl font-semibold text-center">
 				About Us
 		</h3>
-        <div class="text-xl sm:text-2xl md:text-3x text-center pt-10">
-            <p class="text-acc3 py-4 text-[#1e1e1e]">Lumina Voice Private Limited is an emerging BPO services company.</p>
-            <p class="text-acc3 py-4 text-[#1e1e1e]">We specialize in inbound services, providing expert support to our clients. Our mission is to deliver focused customer care. </p>
-            <p class="text-acc3 py-4 text-[#1e1e1e]">By creating efficiency in operations, we aim to increase customer satisfaction. We are dedicated to meeting the needs of each customer.</p>
+        <div class="text-xl sm:text-2xl md:text-3x text-center pt-10 ">
+            <p class="py-4 text-[#1e1e1e]">We specialize in inbound services, providing expert support to our clients. Our mission is to deliver focused customer care. </p>
+            <p class="py-4 text-[#1e1e1e]">Lumina Voice Private Limited is an emerging BPO services company.</p>
+            <p class="py-4 text-[#1e1e1e]">By creating efficiency in operations, we aim to increase customer satisfaction. We are dedicated to meeting the needs of each customer.</p>
         </div>
     </div>
 </SectionWrapper>
